310486209d6a058442047ed0deede2d87df1b9e8
Mikhail Kharitonovich <kharitonovich.m@gmail.com>
2017-05-28 19:26:42 +0300

    initial upload

---
+++ src/input.rs
@@ -0,0 +1,35 @@
+use keys::Keys;
+
+#[derive(Debug)]
+pub struct Input {
+    pub e_type: u16,
+    pub code: u16,
+    pub value: i32
+}
+#[derive(Debug)]
+pub enum EventType {
+    Push,
+    Release
+}
+
+impl Input {
+    pub fn from_read(read: &[u8; 24]) -> Input{
+        Input{
+            e_type: ((read[17] << 1) as u16) + (read[16] as u16),
+            code: ((read[19] << 1) as u16) + (read[18] as u16),
+            value: ((read[23] << 3) as i32) + ((read[22] << 2) as i32) + ((read[21] << 1) as i32) + (read[20]as i32)
+        }
+    }
+    pub fn is_key_event(&self) -> bool{
+        self.e_type == 1
+    }
+    pub fn event_type(&self) -> EventType{
+        match self.value {
+            1 => EventType::Push,
+            _ => EventType::Release
+        }
+    }
+    pub fn get_key(&self) -> Keys{
+        Keys::from_code(self.code)
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+mod input;
+mod keys;
+    
+use std::process::Command;
+use std::str::from_utf8;
+use std::fs::{File, OpenOptions};
+use std::io::Read;
+
+use input::Input;
+
+#[derive(Debug)]
+pub struct InputReader {
+    file: File
+}
+
+impl InputReader {
+    pub fn new(path: String) -> InputReader {
+        InputReader{file: OpenOptions::new().read(true).write(false).append(false).open(path).expect("could not open device file")}
+    }
+    pub fn current_state(&mut self) -> Input{
+        let mut buf: [u8; 24] = [0 as u8; 24];
+        self.file.read(&mut buf).expect("error reading file");
+        Input::from_read(&buf)
+    }
+}
+
+pub fn get_input_devices() -> Vec<String>{
+    let command = "grep -E 'Handlers|EV' /proc/bus/input/devices | grep -B1 120013 | grep -Eo event[0-9]+".to_string();
+    let result = Command::new("sh").arg("-c").arg(command).output().expect("could not execute command to search for input devices");
+    let result_str = from_utf8(&result.stdout).expect("could not get results");
+        
+    result_str.trim().split('\n').map(|s| "/dev/input/".to_string() + s).collect()
+}
